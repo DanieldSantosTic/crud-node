850063144c6e5e114a7ab6942ea74e3814ae2ab1
DanieldSantosTic <daniel.fonseca@ticsocial.com.br>
2021-08-23 11:58:07 -0300

    basic structure
    basic structure

---
+++ queries.js
@@ -0,0 +1,78 @@
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "ecommerce_api",
+  password: "123",
+  port: 5432,
+});
+
+const getUsers = (req, res) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+const createUser = (req, res) => {
+  const { email, username, senha, nome, cpf, data_nascimento } = req.body;
+
+  pool.query(
+    'INSERT INTO cliente (email, username, senha, nome, cpf, data_nascimento) VALUES($1, $2, $3, $4, $5, $6)',
+    [email, username, senha, nome, cpf, data_nascimento],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`User added with ID: ${results.insertId}`);
+    }
+  );
+};
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, email } = req.body;
+
+  pool.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
